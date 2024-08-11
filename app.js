91eb1c3bf1fdd9ef2d5dffff7d841a2d90ddc5c1
NouranMohsen <152489722+NouranMohsen@users.noreply.github.com>
2024-08-11 12:58:39 +0300

    initialize connection

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const express = require('express')
+import  express from 'express'
+import sequelize from './config/dbConn.js'
 const app = express()
+app.use(express.json())
 const port = 3000
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+sequelize.sync().then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  });
---
+++ config/dbConn.js
@@ -0,0 +1,16 @@
+import  Sequelize  from "sequelize";
+
+const sequelize = new Sequelize('store-api','root','',{
+    host:'localhost',
+    dialect : 'mysql',
+    port: 3307
+     
+})
+
+//auth to db
+sequelize.authenticate()
+.then(()=> console.log('db Connected sucessfully...'))
+.catch((err)=> console.log(err));
+
+
+export default sequelize
